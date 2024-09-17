4b45df26fa98cb3365e31f1079f30959205fff26
RamezCh <ramiz_chreide@outlook.com>
2024-09-17 14:32:59 +0300

    Update Files to MongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = callback => {
+  /** REPLACE CONNECTION STRING IF USING ATLAS
+   *  "mongodb+srv://<username>:<password>@<cluster-id>.mongodb.net/<dbName>?retryWrites=true&authSource=admin"
+   */
+  MongoClient.connect(
+    'mongodb://127.0.0.1:27017/shop?retryWrites=true&authSource=admin'
+  )
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
