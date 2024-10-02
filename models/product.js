5b36c1fb0fcc88cc7327481dc347e8311b96cd71
RamezCh <ramiz_chreide@outlook.com>
2024-10-02 16:47:27 +0300

    Creating Products Schema

---
+++ models/product.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  // _id is added automatically as object_id
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
