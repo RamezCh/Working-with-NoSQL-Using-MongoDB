1e9e58cec6f763b290f13bb100a92f3c78d85859
RamezCh <ramiz_chreide@outlook.com>
2024-10-11 22:11:34 +0300

    Using CSRF

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,6 +18,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -35,6 +37,8 @@ app.use(
     store: store,
   })
 );
+// csrf uses session, so we place it after it
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
