d6e2ee78e143d3871416a30f4a0d3765d793516d
RamezCh <ramiz_chreide@outlook.com>
2024-10-11 21:41:21 +0300

    hashing pass with bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -38,7 +39,14 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
-      const user = new User({ email, password, cart: { items: [] } });
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
+      const user = new User({
+        email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
       return user.save();
     })
     .then(result => {
